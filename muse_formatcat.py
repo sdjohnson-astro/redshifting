2931853ce1ed9f05cca648a9822f8405aacb0658
sdjohnson <sdj@astro.princeton.edu>
2021-06-21 09:15:22 -0400

    Added a cosmos3 and MUSE versions of _redshifting.py. Also added a GUI for inspecting/tweaking wavelength calibrations for COSMOS reductions.

--- muse_formatcat.py
+++ muse_formatcat.py
@@ -7,15 +7,28 @@ import coord
 # Set up the command line argument parser
 parser = argparse.ArgumentParser(description='Create a muse initial input catalog')
 parser.add_argument('-f', metavar='catalog filename', type=str, help='name of the catalog to be formatted', required=True)
+parser.add_argument('-r', metavar='radius', type=float, help='Initial extraction radius', required=False, default=0.0)
+parser.add_argument('-c', metavar='caps', type=bool, help='Initial catalog has caps?', required=False, default=False)
+
 args = parser.parse_args()
 
 objects = Table.read(args.f, format='ascii')
 
-ra_string, dec_string, objects['id'] = coord.coordstring(objects['ra'], objects['dec'])
+if args.c:
+   objects.rename_column('ID', 'id')
+   objects.rename_column('RA_J2000', 'ra')
+   objects.rename_column('DEC_J2000', 'dec')
 
-objects['row'] = np.arange(len(objects))
+print(objects)
+ra_string, dec_string, objects['name'] = coord.coordstring(objects['ra'], objects['dec'])
 
-objects = objects['row', 'id', 'ra', 'dec', 'radius']
+objects['row'] = np.arange(len(objects)) + 1
+if args.r == 0:
+   objects['radius'] = objects['r_Kron']*0.2
+else:
+   objects['radius'] = args.r
+objects = objects['row', 'id', 'name', 'ra', 'dec', 'radius']
+
 
 print(objects)
 
